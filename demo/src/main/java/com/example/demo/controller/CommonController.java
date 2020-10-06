df9437adc9332463f6f093601d9840c9ea077fed
rg <lustell@gmail.com>
2020-10-06 09:41:10 +0900

    [2020.10.06] Prepare fundamental elements

---
+++ demo/src/main/java/com/example/demo/controller/CommonController.java
@@ -0,0 +1,17 @@
+package com.example.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class CommonController {
+	@RequestMapping("/")
+	public String home() {
+		return "index";
+	}
+
+	@RequestMapping("/main")
+	public String mainPage() {
+		return "public";
+	}
+}
---
+++ demo/src/main/java/com/example/demo/controller/WorkerController.java
@@ -0,0 +1,14 @@
+package com.example.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/worker/*")
+public class WorkerController {
+	@RequestMapping
+	public String home() {
+		return "worker/worker";
+	}
+
+}
