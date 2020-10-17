79849fe6ee00c03c93bd774062925880e02b9088
Lustellz <45252527+Lustellz@users.noreply.github.com>
2020-10-17 06:00:15 +0000

    Revert "ㅇ"
    This reverts commit fc1ab5cf12a60e80f6a0c1e6108fd83b8808a380.

--- platform/src/main/java/kr/co/dmtlabs/platform/controller/WorkerController.java
+++ platform/src/main/java/kr/co/dmtlabs/platform/controller/WorkerController.java
@@ -10,41 +10,5 @@ public class WorkerController {
 	public String home() {
 		return "worker/worker";
 	}
-	
-	@RequestMapping(value ="JoinForm")
-	public String JoinForm() {
-		//기존에 입력하였던 이력서가 있을시에 가져오기
-		//이력서에 현재까지 참여했던 프로젝트를 이력서에 자동적으로 추가할지 여부 선택
-		return "worker/JoinForm.jsp"
-	}
-	
-	@RequestMapping(value ="MyPage")
-	public String MyPage() {
-		//참여했던 프로젝트 내역
-		//프로젝트에 대한 결과물 내역
-		//진행 중인 프로젝트의 기간
-		//평가 내역
-		//프로젝트 선정 결과 확인
-		return "worker/MyPage.jsp"
-	}
-	
-	@RequestMapping(value ="Person_Info")
-	public String Person_Info() {
-		//가입 당시에 입력하였던 정보 가져오기
-		return "worker/Person_Info.jsp"
-	}
-	
-	@RequestMapping(value ="Point_")
-	public String Point_() {
-		//완료된 프로젝트의 포인트 내역
-		//완료된 프로젝트의 포인트 지급 여부
-		return "worker/Point_.jsp"
-	}
-	
-	@RequestMapping(value ="Project_Result")
-	public String Project_Result() {
-		//해당 프로젝트의 결과물 제출
-		return "worker/Project_Result.jsp"
-	}
-	
+
 }
