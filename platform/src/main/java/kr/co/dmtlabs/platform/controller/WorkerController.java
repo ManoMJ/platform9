265f088ddf64417b692a8ab8c13e9fb3a0cd3c51
HipoHipp <59777321+HipoHipp@users.noreply.github.com>
2020-10-31 07:02:50 +0900

    Repository 부분수정

--- platform/src/main/java/kr/co/dmtlabs/platform/controller/WorkerController.java
+++ platform/src/main/java/kr/co/dmtlabs/platform/controller/WorkerController.java
@@ -13,8 +13,26 @@ public class WorkerController {
 	
 	@RequestMapping
 	public String selectMyInfo() {
-		
+		//select 워커의 개인정보
+		//프로젝트 결과 리스트
+		//작업 중인 프로젝트 리스트
+		//나의 프로젝트 리스트
 		return "worker/selectWorkerInfo";
+	}
+	
+	public String selectResume() {
+		//worker가 등록한 이력서 확인
+		return "worker/selectResume";
+	}
+	
+	public String insertJob_Result() {
+		//worker의 작업에 대한 결과물 제출
+		return "worker/insertJob_Result";
+	}
+	
+	public String selectEvaluation() {
+		//worker의 작업물에 대한 평가 확인 및 worker의 총 평점 확인
+		return "worker/selectEcaluation";
 	}
 
 }
